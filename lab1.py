3ce194e95faa7c7c6105439787d97b7b564f92b9
MinaYimam <77759463+MinaYimam@users.noreply.github.com>
2022-01-13 14:50:42 -0600

    lab1

---
+++ lab1.py
@@ -0,0 +1,10 @@
+name = input('what is you name?')
+birthdaymonth = input('what month were you born?')
+if birthdaymonth == 'january':
+    print('happy birthday ' + name)
+else:
+    print('happy birthday later this year')
+
+
+
+
---
+++ lab2.py
@@ -0,0 +1,13 @@
+classes = []
+numberofclasses = int(input('how many classes are you taking?'))
+
+
+
+while true:
+     classname = ('enter the name of your class or enter to quit')
+
+
+a = int(input('how many classes are you taking?'))
+for i in range (a):
+    i = input('what class are you taking?')
+    if input =
