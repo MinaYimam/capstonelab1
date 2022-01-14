d92e0e3e99f84d5036e5a9a8fd92beabf6133871
MinaYimam <77759463+MinaYimam@users.noreply.github.com>
2022-01-14 14:13:25 -0600

    lab2

--- lab2.py
+++ lab2.py
@@ -1,13 +1,27 @@
-classes = []
-numberofclasses = int(input('how many classes are you taking?'))
+class_number= int(input('how many classes are you taking?'))
+#ask how many classes is a student is taking
+for i in range (class_number):
+    #loop on the question for how many classes the student enteres
+        classname =input('what class are you taking?')
+    #ask the student the classes the student is taking
+       # if not classname:
 
 
 
-while true:
-     classname = ('enter the name of your class or enter to quit')
+
+# break
+#classes = []
+#numberofclasses = int(input('how many classes are you taking?'))
+
+#while True:
+#     classname = input('enter the name of your class or enter to quit: ')
+   #  if not classname:
+#         break
+#         classes.append(classname)
+#for c in classes:
+  #  print(c)
 
 
-a = int(input('how many classes are you taking?'))
-for i in range (a):
-    i = input('what class are you taking?')
-    if input =
+
+
+  #  if input =
